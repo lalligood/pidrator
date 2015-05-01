0d154b699050b52a5460c96c5e3e1f1b1639f357
Lance Alligood <lalligood@gmail.com>
2015-05-01 13:11:55 -0400

    Started on user input menu.

--- pidrator.py
+++ pidrator.py
@@ -14,6 +14,7 @@ def read_temp_raw():
     f.close()
     return lines
 
+
 def read_temp():
     lines = read_temp_raw()
     while lines[0].strip()[-3:] != 'YES':
@@ -26,14 +27,17 @@ def read_temp():
         temp_f = temp_c * 9.0 / 5.0 + 32.0
         return temp_c, temp_f
 
+
 def userquit():
     msg = "Terminating pidrator."
     logging.warning(msg)
     print msg
     sys.exit(0)
 
+
 def userwait():
     time.sleep(5)
+
 
 def enable():
     # Configure powertail & begin data collection
@@ -46,7 +50,7 @@ def enable():
     base_dir = '/sys/bus/w1/devices/'
     #device_folder = glob.glob(base_dir + '28*')[0]
     #device_file = device_folder + '/w1_slave'
-    device_file = False
+    device_file = False # This added to ensure following condition is false
     if device_file:
         msg = 'Powertail detected & started successfully.'
     else:
@@ -54,17 +58,48 @@ def enable():
     logging.info(msg)
     print msg
 
+
 def poweron():
     #io.output(power_pin, True)
     msg = 'Powertail turned ON successfully.'
     logging.info(msg)
     print msg
 
+
 def poweroff():
     #io.output(power_pin, False)
     msg = 'Powertail turned OFF successfully.'
     logging.info(msg)
     print msg
+
+
+def selectmenu():
+    #os.system('clear') # clear screen
+    print 'Welcome to pidrator'
+    print '1. Dehydrator'
+    print '2. Slow Cooker'
+    cookdevice = int(raw_input('Enter the number of the device you want to cook with: '))
+    if cookdevice == 1:
+        print 'You selected Dehydrator.'
+    elif cookdevice == 2:
+        print 'You selected Slow Cooker.'
+    else:
+        print 'That is not a valid selection. Please try again...'
+        time.sleep(5)
+        selectmenu()
+    cookfood = raw_input('What are you planning to cook? ')
+    if cookfood == '':
+        print 'Please enter what you are planning to cook...'
+        time.sleep(5)
+        selectmenu()
+    cookhour = int(raw_input('Enter the number of hours would you like to cook: '))
+    if cookhour > 2 or cookhour <= 12:
+        print 'You want to cook something for', cookhour, 'hours.'
+    else:
+        print 'You have entered a time that is too short or too long. Please try again...'
+        time.sleep(5)
+        selectmenu()
+
 
 # Configure logging
 logfilename = 'pidrator.log'
@@ -76,9 +111,9 @@ logging.basicConfig(filename=logfilename, level=loglevel, format=logformat, date
 # Main routine
 logging.info('Starting up pidrator engine.')
 enable()
+selectmenu()
 count = 0
 header = 'Temp_C Temp_F Date_Time                Count'
-print
 while True:
     if count % 10 == 0:
         print header
