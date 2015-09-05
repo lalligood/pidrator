ad1964e4d50ee69f5b478c47aad1dc5bf6706936
Lance Alligood <lalligood@gmail.com>
2015-09-05 08:39:08 -0400

    psycopg2: Further refinement of sql_test.py with more variables

--- sql_test.py
+++ sql_test.py
@@ -3,19 +3,28 @@
 import psycopg2
 import sys
 
+# DB connection info
+mydb = 'postgres'
+mydbuser = 'lalligood'
+mydbport = 5433
+
 # Connect to DB
-conn = psycopg2.connect('dbname=postgres user=lalligood port=5433')
+conn = psycopg2.connect(database=mydb, user=mydbuser, port=mydbport)
 cur = conn.cursor()
 
-cur.execute('select * from users')
-print(cur.fetchall())
-input()
+#SQL = 'insert into devices (devicename) values (%s) returning *'
+#devname = ('stove', )
+#cur.execute(SQL, devname)
+#conn.commit()
+SQL = 'select username, fullname, email_address from users;'
+cur.execute(SQL)
+row = cur.fetchone()
+print('Your username is: ' + row[0])
+print('Your name is: ' + row[1])
+print('Your email address is: ' + row[2])
+print('But I\'m not telling you what your password is!')
 
-SQL = 'insert into devices (devicename) values (%s) returning *'
-devname = ('stove', )
-cur.execute(SQL, devname)
-conn.commit()
-print(cur.fetchall())
+# Close DB connection & exit gracefully
 cur.close()
 conn.close()
 sys.exit()
