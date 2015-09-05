8bb0b955b80fe8c1fcd8e2ea4056f2c96b6d034a
Lance Alligood <lalligood@gmail.com>
2015-09-05 08:49:15 -0400

    psycopg2: +import datetime & extended data retrieval

--- sql_test.py
+++ sql_test.py
@@ -2,6 +2,7 @@
 
 import psycopg2
 import sys
+import datetime
 
 # DB connection info
 mydb = 'postgres'
@@ -12,10 +13,13 @@ mydbport = 5433
 conn = psycopg2.connect(database=mydb, user=mydbuser, port=mydbport)
 cur = conn.cursor()
 
+# INSERT
 #SQL = 'insert into devices (devicename) values (%s) returning *'
 #devname = ('stove', )
 #cur.execute(SQL, devname)
 #conn.commit()
+
+# SELECT w/o WHERE
 SQL = 'select username, fullname, email_address from users;'
 cur.execute(SQL)
 row = cur.fetchone()
@@ -23,6 +27,15 @@ print('Your username is: ' + row[0])
 print('Your name is: ' + row[1])
 print('Your email address is: ' + row[2])
 print('But I\'m not telling you what your password is!')
+
+# SELECT w/ WHERE
+SQL = 'select * from devices where devicename = (%s)'
+devname = ('slow cooker', )
+cur.execute(SQL, devname)
+row = cur.fetchone()
+print('slow cooker ID is: ' + row[0])
+created = row[2].strftime('%m-%d-%Y %H:%M:%S')
+print('slow cooker was added to DB on: ' + created)
 
 # Close DB connection & exit gracefully
 cur.close()
