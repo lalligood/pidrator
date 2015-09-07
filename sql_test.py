c7df0bffa7ff10fc5d6c6d942fe0952959415c5c
Lance Alligood <lalligood@gmail.com>
2015-09-06 23:50:37 -0400

    python: First steps of taking user input for row to be modified

--- sql_test.py
+++ sql_test.py
@@ -12,7 +12,7 @@ def cleanexit(): # Close DB connection & exit gracefully
     conn.close()
     sys.exit()
 
-def query():
+def query(): # General purpose query submission that will exit if error
     try:
         cur.execute(SQL, params)
     except psycopg2.Error as dberror:
@@ -62,19 +62,34 @@ print('slow cooker was added to DB on: ' + created)
 # SELECT W/ JOIN
 # INSERT new job_info row first!
 '''
-SQL = 'insert into job_info (job_name) values (%s) returning *'
+SQL = 'insert into job_info (jobname) values (%s) returning *'
 params = ('first time', )
 query()
 conn.commit()
 '''
 
-# SELECT username from job_info
-getjob = input()
-jobname = '(\'' + getjob + '\', )'
-SQL = 'select id from users where jobname = (%s)'
-params = ('lalligood', )
+# Retrieve list of job names from job_info
+print('Here is a list of job names: ')
+SQL = 'select jobname from job_info'
+params = ''
 query()
-user = cur.fetchone()
+jobname = cur.fetchall()
+for x in jobname:
+    print(x)
+
+# Get user input & SELECT row from job_info based on input
+print('Enter the name of the job that you want to modify: ')
+response = input()
+# Format response appropriately as variable 
+jobname = eval('(\'' + response + '\', )')
+# Check to see if response matches result(s) here
+'''
+'''
+# Fetch job ID for selected job
+SQL = 'select id from job_info where jobname = (%s)'
+params = jobname
+query()
+jobid = cur.fetchone()
 
 # SELECT username from users
 SQL = 'select id from users where username = (%s)'
@@ -88,10 +103,8 @@ params = ('slow cooker', )
 query()
 device = cur.fetchone()
 
-print (jobname, user, device)
-
 # UPDATE user_id & device_id in job_info
-SQL = 'update job_info set user_id = (%s), device_id = (%s) where job_name = (%s)'
+SQL = 'update job_info set user_id = (%s), device_id = (%s) where jobname = (%s)'
 params = user + device + jobname
 query()
 conn.commit()
