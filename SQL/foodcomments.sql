4616c9b8208cec3f2617e0cd0f29df3b8daf5046
Lance Alligood <lalligood@gmail.com>
2015-09-07 11:58:58 -0400

    SQL: renamed createtime column in foodcomments table
    Was originall given wrong name

--- SQL/foodcomments.sql
+++ SQL/foodcomments.sql
@@ -3,5 +3,5 @@ drop table if exists foodcomments;
 create table foodcomments (
     jobinfo_id uuid not null
     , foodcomments text
-    , createdate timestamp with time zone default now()
+    , createtime timestamp with time zone default now()
     );
