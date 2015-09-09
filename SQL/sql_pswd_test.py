659fd3139bf98e7e4c9d909ec4d80aa15f722006
Lance Alligood <lalligood@gmail.com>
2015-09-08 23:10:16 -0400

    python: sql_pswd_test.py is all about user passwords
    getpass library used to suppress user password entry on screen.
    
    Successfully verifies user/pswd then changes password through update.
    
    Updating password has to pass 3 criteria first.

---
+++ SQL/sql_pswd_test.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+__author__ = 'lalligood'
+
+import psycopg2
+import psycopg2.extras
+import sys
+import datetime
+import time
+import getpass
+
+psycopg2.extras.register_uuid()
+
+def cleanexit(): # Close DB connection & exit gracefully
+    cur.close()
+    conn.close()
+    sys.exit()
+
+def query(SQL, params): # General purpose query submission that will exit if error
+    try:
+        cur.execute(SQL, params)
+    except psycopg2.Error as dberror:
+        print(dberror.diag.severity + ' - ' + dberror.diag.message_primary)
+        cleanexit()
+
+def userlogin(user): # User login
+    response = getpass.getpass('Enter your password: ')
+    pswd = eval('(\'' + response + '\', )')
+    SQL = 'select (password = crypt((%s), password)) as userpass from users where username = (%s)'
+    params = pswd + user
+    query(SQL, params)
+    pswdverify = cur.fetchone()
+    if pswdverify[0]:
+        print('Password entered successfully!')
+    else:
+        print('Username and/or password incorrect. Try again...')
+        time.sleep(2)
+        userlogin(user)
+
+def changepswd(user): # User elects to change password
+    response = getpass.getpass('Enter your current password: ')
+    oldpswd = eval('(\'' + response + '\', )')
+    response = getpass.getpass('Enter your new password: ')
+    newpswd1 = eval('(\'' + response + '\', )')
+    response = getpass.getpass('Enter your new password again: ')
+    newpswd2 = eval('(\'' + response + '\', )')
+    if newpswd1[0] != newpswd2[0]:
+        print('New passwords do not match. Try again...')
+        changepswd(user)
+    if len(newpswd1[0]) < 8:
+        print('New password length is too short. Try again...')
+        changepswd(user)
+    if oldpswd[0] == newpswd1[0]:
+        print('New password must be different from old password. Try again...')
+        changepswd(user)
+    SQL = 'select (password = crypt((%s), password)) as userpass from users where username = (%s)'
+    params = oldpswd + user
+    query(SQL, params)
+    pswdverify = cur.fetchone()
+    if pswdverify[0]:
+        SQL = 'update users set password = crypt((%s), gen_salt(\'bf\')) where username = (%s)'
+        params = newpswd1 + user
+        query(SQL, params)
+        conn.commit()
+        print('Your password has been updated successfully.')
+    else:
+        print('Username and/or password incorrect. Try again...')
+        time.sleep(2)
+        changepswd(user)
+
+# DB connection info
+mydb = 'postgres'
+mydbuser = 'lalligood'
+mydbport = 5433
+
+# Connect to DB
+conn = psycopg2.connect(database=mydb, user=mydbuser, port=mydbport)
+cur = conn.cursor()
+
+response = input('Enter your username: ')
+user = eval('(\'' + response + '\', )')
+userlogin(user)
+changepswd(user)
