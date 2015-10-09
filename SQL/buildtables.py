d02fad508759e4d178b33735162b5d46dee56717
Lance Alligood <lalligood@gmail.com>
2015-10-08 21:48:19 -0400

    buildtables: +error handling if cannot create DB extensions
    +minor changes to log file & log message format

--- SQL/buildtables.py
+++ SQL/buildtables.py
@@ -160,9 +160,9 @@ else: # NON-RASPI TEST DB
 # For inserting dates to DB & for logging
 date_format = '%Y-%m-%d %H:%M:%S' # YYYY-MM-DD HH:MM:SS
 # Logging information
-logfilename = 'sql_test.log'
+logfilename = 'buildtables.log'
 loglevel = logging.WARNING # Available logging levels, from low to high: DEBUG, INFO, WARNING, ERROR, CRITICAL
-logformat = '%(time.asctime)s %(levelname)s: %(message)s'
+logformat = '%(time.asctime)s [%(levelname)s] %(message)s'
 logging.basicConfig(filename=logfilename, level=loglevel, format=logformat, datefmt=date_format)
 logging.info('Initializing application & attempting to connect to database.')
 # Hardware configuration
@@ -178,28 +178,30 @@ try:
     cur = conn.cursor()
     logging.info('Connected to database successfully')
 except psycopg2.Error as dberror:
-    logging.critical('Unable to connect to database. Is it running?')
+    logging.critical('UNABLE TO CONNECT TO DATABASE. Is it running?')
     cleanexit(1)
 
 # Check to see if all tables exists
 schema = ('public', )
 tables = query('select \
-table_name \
-from information_schema.tables \
-where table_schema = (%s) \
-order by table_name', schema, 'all', False)
+    table_name \
+    from information_schema.tables \
+    where table_schema = (%s) \
+    order by table_name', schema, 'all', False)
 tables_list = [] # Convert results tuple -> list
 for table in tables:
     tables_list.append(table[0])
 master_list = ['devices', 'foodcomments', 'foods', 'job_data', 'job_info', 'users']
 # Get difference of master_list & tables_list
 results_list = set(master_list).difference(tables_list)
+# Create any tables that do not exist
 if len(results_list) > 0:
-# If either of the following lines fail, then it should return error
-# message & terminate that postgres-contrib-9.x and/or postgres-9.4-pgcrypto
-# need to be installed.
-    query('create extension if not exists "uuid-ossp";', None, '', False)
-    query('create extension if not exists "pgcrypto";', None, '', False)
+    try:
+        query('create extension if not exists "uuid-ossp";', None, '', False)
+        query('create extension if not exists "pgcrypto";', None, '', False)
+    except psycopg2.Error as dberror:
+        logging.critical("Unable to create PostgreSQL extensions. Run 'apt-get install postgresql-contrib-9.4'.")
+        cleanexit(1)
     for result in results_list:
         options = {
             'devices' : create_devices,
